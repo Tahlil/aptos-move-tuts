09e5a4120f40325b34b60d0eb6a2ac59ddba1de6
Tahlil <bsse0803@iit.du.ac.bd>
2023-09-25 20:40:38 +0600

    WIP: Create modular create collection function

--- nft.js
+++ nft.js
@@ -11,8 +11,9 @@ const aptosCoin = "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>";
   const client = new aptos.AptosClient(NODE_URL);
   const faucetClient = new aptos.FaucetClient(NODE_URL, FAUCET_URL, null);
   const tokenClient = new aptos.TokenClient(client);
-
-  const account1 = new aptos.AptosAccount();
+  const privateKey = process.env.PRIVATE_KEY;
+  const privKeyInBytes = Uint8Array.from(Array.from(privateKey).map(letter => letter.charCodeAt(0)));
+  const account1 = new aptos.AptosAccount(privKeyInBytes);
   await faucetClient.fundAccount(account1.address(), 100_000_000);
   let resources = await client.getAccountResources(account1.address());
   let accountResource = resources.find((r) => r.type === aptosCoin);
@@ -23,7 +24,7 @@ const aptosCoin = "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>";
   resources = await client.getAccountResources(account2.address());
   accountResource = resources.find((r) => r.type === aptosCoin);
   console.log(`account2 coins: ${accountResource.data.coin.value}. Should be 0!`);
-
+  account2.address()
   const payload = {
     type: "entry_function_payload",
     function: "0x1::coin::transfer",
@@ -34,7 +35,8 @@ const aptosCoin = "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>";
   const signedTxn = await client.signTransaction(account1, txnRequest);
   const transactionRes = await client.submitTransaction(signedTxn);
   await client.waitForTransaction(transactionRes.hash);
-
+  console.log(account2.address());
+  console.log(account1.address());
   resources = await client.getAccountResources(account2.address());
   accountResource = resources.find((r) => r.type === aptosCoin);
   console.log(`account2 coins: ${accountResource.data.coin.value}. Should be 717!`);
@@ -54,16 +56,16 @@ const aptosCoin = "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>";
 
   console.log("=== Creating account1's NFT Collection and Token ===");
 
-  const collectionName = "Alice's";
-  const tokenName = "Alice's first token";
 
-  // Create the collection.
+
+async function createCollection(collectionName, tokenName) {
+    // Create the collection.
   // :!:>section_4
   const txnHash1 = await tokenClient.createCollection(
     account1,
     collectionName,
-    "Alice's simple collection",
-    "https://alice.com",
+    "Tahlil's second collection",
+    "https://static.wikia.nocookie.net/pokemonfanon/images/4/48/Swampert_9.jpg/revision/latest",
   ); // <:!:section_4
   await client.waitForTransaction(txnHash1, { checkSuccess: true });
 
@@ -73,12 +75,15 @@ const aptosCoin = "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>";
     account1,
     collectionName,
     tokenName,
-    "Alice's simple token",
+    "Tahlil's simple token",
     1,
-    "https://aptos.dev/img/nyan.jpeg",
+    "https://static.wikia.nocookie.net/pokemonfanon/images/4/48/Swampert_9.jpg/revision/latest",
   ); // <:!:section_5
   await client.waitForTransaction(txnHash2, { checkSuccess: true });
 
   const nfts = await provider.getAccountNFTs(account1.address().hex());
   console.log(`account1 current token ownership: ${nfts.current_token_ownerships[0].amount}. Should be 1`);
+}
+createCollection("Tahlil's Second collection", "Tahlil's token")
+  
 })();
